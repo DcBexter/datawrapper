5f49a7658c8fb6acb1f24d6b8928831697e52510
DcBexter <dcbexter@gmx.net>
2012-11-23 16:03:00 +0100

    reworked deutsche welle theme

--- www/static/themes/deutsche-welle/theme.js
+++ www/static/themes/deutsche-welle/theme.js
@@ -1,17 +1,12 @@
 (function(){
 
-    // Default Theme
+    // Deutsche Welle Theme
     // -------------
 
-    // Every theme will inherit the properties of this
-    // theme. They can override everything or just a bit
-    // of them. Also, every theme can extend any other
-    // existing theme.
+    Datawrapper.Themes.DeutscheWelle = $.extend(true, {}, Datawrapper.Themes.Base, {
 
-    Datawrapper.Themes.DeutscheWelle = _.extend({}, Datawrapper.Themes.Base, {
-
-        hpadding: 20,
-        vpadding: -10,
+        hpadding: 0,
+        vpadding: 10,
 
         colors: {
             palette: ['#DC0F6E', '#009BFF', '#82B905', '#82141E', '#E14614', '#C8D205', '#EB8C14'],
