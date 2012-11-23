77d285de5c3c89960a6d13326533d91044f7856c
DcBexter <dcbexter@gmx.net>
2012-11-23 15:28:35 +0100

    added source field to be required

--- controller/chart-visualize.php
+++ controller/chart-visualize.php
@@ -8,20 +8,30 @@ require_once '../lib/utils/themes.php';
  * VISUALIZE STEP
  */
 $app->get('/chart/:id/visualize', function ($id) use ($app) {
-    check_chart_writable($id, function($user, $chart) use ($app) {
-        $page = array(
-            'chartData' => $chart->loadData(),
-            'chart' => $chart,
-            'visualizations' => get_visualizations_meta('', true),
-            'vis' => get_visualization_meta($chart->getType()),
-            'themes' => get_themes_meta()
-        );
-        add_header_vars($page, 'chart');
-        add_editor_nav($page, 3);
-        $res = $app->response();
-        $res['Cache-Control'] = 'max-age=0';
+        check_chart_writable($id, function($user, $chart) use ($app) {
+                $page = array(
+                    'chartData' => $chart->loadData(),
+                    'chart' => $chart,
+                    'visualizations' => get_visualizations_meta('', true),
+                    'vis' => get_visualization_meta($chart->getType()),
+                    'themes' => get_themes_meta()
+                );
 
-        $app->render('chart-visualize.twig', $page);
+                $metaData = $chart->getMetadata();
+                if (!isset($metaData['describe']['source-name']) || !isset($metaData['describe']['source-url']) || $metaData['describe']['source-name'] == '' || $metaData['describe']['source-url'] == '') {
+                    add_header_vars($page, 'chart');
+                    add_editor_nav($page, 2);
+                    $res = $app->response();
+                    $res['Cache-Control'] = 'max-age=0';
+                    $app->render('chart-describe-error-source.twig', $page);
+                } else {
+                    add_header_vars($page, 'chart');
+                    add_editor_nav($page, 3);
+                    $res = $app->response();
+                    $res['Cache-Control'] = 'max-age=0';
+
+                    $app->render('chart-visualize.twig', $page);
+                }
+            });
     });
-});
 
